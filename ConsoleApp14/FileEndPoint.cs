a23282ab6a8c4a623c6f2c0590721ec2e6611d8a
Peter Levinsky <pele@easj.dk>
2020-10-20 22:11:41 +0200

    Add project files.

---
+++ ConsoleApp14/FileEndPoint.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp14
+{
+    public class FileEndPoint
+    {
+        private string _ipAddress;
+        private int _port;
+
+        public string IpAddress
+        {
+            get => _ipAddress;
+            set => _ipAddress = value;
+        }
+
+        public int Port
+        {
+            get => _port;
+            set => _port = value;
+        }
+
+        public FileEndPoint()
+        {
+        }
+
+        public FileEndPoint(string ipAddress, int port)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(IpAddress)}: {IpAddress}, {nameof(Port)}: {Port}";
+        }
+
+        protected bool Equals(FileEndPoint other)
+        {
+            return _ipAddress == other._ipAddress && _port == other._port;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((FileEndPoint) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((_ipAddress != null ? _ipAddress.GetHashCode() : 0) * 397) ^ _port;
+            }
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Dictionary<String, List<FileEndPoint>> dic = new Dictionary<String, List<FileEndPoint>>();
+            String filename = "peter.txt";
+            FileEndPoint fep1 = new FileEndPoint("localhost", 3030);
+            FileEndPoint fep2 = new FileEndPoint("localhost", 3031);
+            FileEndPoint fep3 = new FileEndPoint("localhost", 3032);
+            FileEndPoint fep4 = new FileEndPoint("localhost", 3033);
+
+            InsertIntoDictionay(dic, filename, fep1); // key findes ikke i forvejen
+            InsertIntoDictionay(dic, filename, fep2); // key findes allerede
+
+
+            DeleteFromDictionay(dic, filename, fep1);
+            DeleteFromDictionay(dic, "fileFindesIkke.txt", fep1);
+
+            List<FileEndPoint> liste = SearchInDictionary(dic, filename);
+            foreach (FileEndPoint endPoint in liste)
+            {
+                Console.WriteLine(endPoint);
+            }
+
+            InsertIntoDictionay(dic, filename, fep3);
+            InsertIntoDictionay(dic, filename, fep4);
+            List<FileEndPoint> newliste = SearchInDictionary(dic, "peter.txt");
+            foreach (FileEndPoint endPoint in newliste)
+            {
+                Console.WriteLine(endPoint);
+            }
+
+
+            Console.WriteLine("Så vi færdige!");
+        }
+
+        private static void InsertIntoDictionay(Dictionary<string, List<FileEndPoint>> dic, string filename, FileEndPoint fep)
+        {
+            /*
+             * insert into Dictionary
+             */
+
+            if (dic.ContainsKey(filename))
+            {
+                // key does exists
+                List<FileEndPoint> listOfFep = dic[filename];
+                listOfFep.Add(fep);
+            }
+            else
+            {
+                // key do not exists
+                List<FileEndPoint> listOfFep = new List<FileEndPoint>();
+                listOfFep.Add(fep);
+                dic.Add(filename, listOfFep);
+            }
+        }
+
+
+        private static void DeleteFromDictionay(Dictionary<string, List<FileEndPoint>> dic,
+                                                String filename, FileEndPoint fep)
+        {
+
+            /*
+            * Delete from dictionary
+            */
+
+            
+            if (dic.ContainsKey(filename))
+            {
+                // Key does exists
+                List<FileEndPoint> listOfFep = dic[filename];
+                listOfFep.RemoveAll(f => f.IpAddress == fep.IpAddress &&
+                                         f.Port == fep.Port);
+                
+                // hvis equals er overskrevet i FileEndPoint klassen 
+                //listOfFep.Remove(fep);
+            }
+            else
+            {
+                // Key do not exists
+                // tja så kan vi ikke gøre noget
+            }
+
+        }
+
+
+
+
+        private static List<FileEndPoint> SearchInDictionary(Dictionary<string, List<FileEndPoint>> dic,
+            String filename)
+        {
+
+            if (dic.ContainsKey(filename))
+            {
+                return dic[filename];
+            }
+
+            // returnere en tom liste
+            return new List<FileEndPoint>();
+        }
+    }
+}
